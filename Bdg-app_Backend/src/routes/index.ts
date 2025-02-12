83c8e38b54d9afe1eebd01025bf19808e07bff73
Tybouddha <bruneecedric@gmail.com>
2025-02-12 07:49:53 +0100

    type debug

--- Bdg-app_Backend/src/routes/index.ts
+++ Bdg-app_Backend/src/routes/index.ts
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 import { Request, Response } from "express";
+
 /* GET home page. */
 router.get("/", function (req: Request, res: Response, next: any) {
   res.render("index", { title: "Express" });
--- Bdg-app_Backend/src/routes/users.ts
+++ Bdg-app_Backend/src/routes/users.ts
@@ -1,7 +1,6 @@
 var express = require("express");
 var router = express.Router();
 import { Request, Response } from "express";
-
 /* GET users listing. */
 router.get("/", function (req: Request, res: Response, next: any) {
   res.send("respond with a resource");
